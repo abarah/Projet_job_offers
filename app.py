93345c830d758e41d956e938e14edbc007696a92
abarah <108078660+abarah@users.noreply.github.com>
2024-12-01 15:42:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-import os
 import re
-import time
-import json
-import pandas as pd
-from flask import Flask, render_template, request, redirect
 from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+from cassandra.query import SimpleStatement
+from nltk.corpus import stopwords
+import os
+import pandas as pd
+from flask import Flask,render_template,redirect,request,session
+from resume_screening import resparser, match
+from lxml import html
 
 # Initialisation de Flask
 app = Flask(__name__)
