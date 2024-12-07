c935cb580720a457bf6e1405f06067e7fd32fa1c
abarah <108078660+abarah@users.noreply.github.com>
2024-12-07 20:32:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import re
-import nltk
 from cassandra.cluster import Cluster
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 from cassandra.query import SimpleStatement
-from nltk.corpus import stopwords
 import os
 import pandas as pd
 from flask import Flask,render_template,redirect,request,session
-from resume_screening import resparser, match
 from lxml import html
 
 
